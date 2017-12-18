d9d6f56c3b835717508ff51f8187dfcb18d3e08e
ppmajchrzak@gmail.com <ppmajchrzak@gmail.com>
2017-12-18 22:38:54 +0100

    chages

--- src/tweet.php
+++ src/tweet.php
@@ -1,129 +1,140 @@
 <?php
-    class Tweet
-    {
-        private $id;
-        private $content;
-        private $user_id;
-        private $userName = ''; //to get owner of tweet
-        private $creationDate;
-        public static function loadTweetById(PDO $conn, $id) {
-            $query = "SELECT * FROM Tweets WHERE id = :id";
-            $stmt = $conn->prepare($query);
-            $result = $stmt->execute([':id' => $id]);
-            if ($result && $stmt->rowCount() == 1) {
-                $currentTweet = $stmt->fetch(PDO::FETCH_ASSOC);
-                $tweet = new Tweet();
-                $tweet->id = $currentTweet['id'];
-                $tweet->content = $currentTweet['content'];
-                $tweet->user_id = $currentTweet['user_id'];
-                $tweet->creationDate = $currentTweet['posted_time'];
-                return $currentTweet;
-            } else {
-                return NULL;
-            }
+class Tweet
+{
+    private $id;
+    private $content;
+    private $user_id;
+    private $userName = '';
+    private $creationDate;
+
+    public static function loadTweetById(PDO $conn, $id) {
+        $query = "SELECT * FROM Tweets WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([':id' => $id]);
+        if ($result && $stmt->rowCount() == 1) {
+            $currentTweet = $stmt->fetch(PDO::FETCH_ASSOC);
+            $tweet = new Tweet();
+            $tweet->id = $currentTweet['id'];
+            $tweet->content = $currentTweet['content'];
+            $tweet->user_id = $currentTweet['user_id'];
+            $tweet->creationDate = $currentTweet['posted_time'];
+            return $currentTweet;
+        } else {
+            return NULL;
         }
-        public static function loadAllTweetsByUserId(PDO $conn, $user_id) {
-            $query = "SELECT * FROM Users  JOIN Tweets ON Users.id=Tweets.user_id WHERE user_id = :user_id";
-            $stmt = $conn->prepare($query);
-            $result = $stmt->execute([':user_id' => $user_id]);
-            $tweets = [];
-            if ($result) {
-                if($stmt->rowCount() > 0)  {
-                    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $currentTweet) {
-                        $tweet = new Tweet();
-                        $tweet->id = $currentTweet['id'];
-                        $tweet->content = $currentTweet['content'];
-                        $tweet->user_id = $currentTweet['user_id'];
-                        $tweet->creationDate = $currentTweet['posted_time'];
-                        $tweets [] = $tweet;
-                    }
-                } else {
-                    $tweets [] = "Nie masz jeszcze żadnego tweeta..";
-                }
-                return $tweets;
-            }
-        }
-        public static function loadAllTweets(PDO $conn) {
-            $query = "SELECT Tweets.id, user_id, username, content, posted_time FROM Users JOIN Tweets WHERE Users.id=Tweets.user_id ORDER BY posted_time DESC";
-            $result = $conn->query($query);;
-            $tweets = [];
-            if ($result && $result->rowCount() > 0) {
-                foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $item) {
+    }
+
+    public static function loadAllTweetsByUserId(PDO $conn, $user_id) {
+        $query = "SELECT * FROM Users  JOIN Tweets ON Users.id=Tweets.user_id WHERE user_id = :user_id";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([':user_id' => $user_id]);
+        $tweets = [];
+        if ($result) {
+            if($stmt->rowCount() > 0)  {
+                foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $currentTweet) {
                     $tweet = new Tweet();
-                    $tweet->id = $item['id'];
-                    $tweet->content = $item['content'];
-                    $tweet->user_id = $item['user_id'];
-                    $tweet->creationDate = $item['posted_time'];
-                    $tweet->userName = $item['username'];
+                    $tweet->id = $currentTweet['id'];
+                    $tweet->content = $currentTweet['content'];
+                    $tweet->user_id = $currentTweet['user_id'];
+                    $tweet->creationDate = $currentTweet['posted_time'];
                     $tweets [] = $tweet;
                 }
+            } else {
+                $tweets [] = "Nie masz jeszcze żadnego tweeta..";
             }
             return $tweets;
         }
-        /**
-         * Tweet constructor.
-         */
-        public function __construct()
-        {
-            $this->id = -1;
-            $this->content = '';
-            $this->user_id = '';
-            $this->creationDate = '';
-            $this->userName = '';
-        }
-        /**
-         * @return mixed
-         */
-        public function getId()
-        {
-            return $this->id;
-        }
-        /**
-         * @return mixed
-         */
-        public function getContent()
-        {
-            return $this->content;
-        }
-        /**
-         * @return mixed
-         */
-        public function getUserName()
-        {
-            return $this->userName;
-        }
-        /**
-         * @return mixed
-         */
-        public function getCreationDate()
-        {
-            return $this->creationDate;
-        }
-        /**
-         * @param mixed $tweet
-         */
-        public function setContent($tweet)
-        {
-            $this->content = $tweet;
-        }
-        /**
-         * @param mixed $user_id
-         */
-        public function setUserId($user_id)
-        {
-            $this->user_id = $user_id;
-        }
-        public function saveToDB(PDO $conn)
-        {
-            $query = "INSERT INTO Tweets(content, user_id, posted_time)
-                      VALUES (:content, :user_id, NOW())";
-            $stmt = $conn->prepare($query);
-            $result = $stmt->execute([
-               ':content' => $this->content,
-               ':user_id' => $this->user_id,
-            ]);
-            if ($result) {
-                return true;
-           }
-        }
     }
+
+    public static function loadAllTweets(PDO $conn) {
+        $query = "SELECT Tweets.id, user_id, username, content, posted_time FROM Users JOIN Tweets WHERE Users.id=Tweets.user_id ORDER BY posted_time DESC";
+        $result = $conn->query($query);;
+        $tweets = [];
+        if ($result && $result->rowCount() > 0) {
+            foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $item) {
+                $tweet = new Tweet();
+                $tweet->id = $item['id'];
+                $tweet->content = $item['content'];
+                $tweet->user_id = $item['user_id'];
+                $tweet->creationDate = $item['posted_time'];
+                $tweet->userName = $item['username'];
+                $tweets [] = $tweet;
+            }
+        }
+        return $tweets;
+    }
+
+    /**
+     * Tweet constructor.
+     */
+    public function __construct()
+    {
+        $this->id = -1;
+        $this->content = '';
+        $this->user_id = '';
+        $this->creationDate = '';
+        $this->userName = '';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param mixed $tweet
+     */
+    public function setContent($tweet)
+    {
+        $this->content = $tweet;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    public function saveToDB(PDO $conn)
+    {
+        $query = "INSERT INTO Tweets(content, user_id, posted_time)
+                  VALUES (:content, :user_id, NOW())";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([
+           ':content' => $this->content,
+           ':user_id' => $this->user_id,
+        ]);
+        if ($result) {
+            return true;
+       }
+    }
+}
--- src/user.php
+++ src/user.php
@@ -1,123 +1,93 @@
 <?php
-
 class User
 {
+
     private $id = -1;
     private $username;
     private $email;
-    private $hashPass;
+    private $hashPasswd;
 
+    public static function loginUser(PDO $conn, $login, $passwd) {
+        $query = "SELECT * FROM Users WHERE email = :login";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([
+            ':login' => $login
+        ]);
+        if ($result && $stmt->rowCount() == 1) {
+            $loggedUser = $stmt->fetch(PDO::FETCH_ASSOC);
+            if (password_verify($passwd, $loggedUser['hash_pass'])) {
+                $user = new User();
+                $user->id = $loggedUser['id'];
+                $user->username = $loggedUser['username'];
+                $user->email = $loggedUser['email'];
+                $_SESSION['loggedUser'] = $user;
+                header("Location:index.php");
+            } else {
+                $_SESSION['msg'] = 'Podane hasło jest niepoprawne';
+                header("Location:index.php");
+            }
+        } else {
+            $_SESSION['msg'] = "W bazie danych nie ma użytkownika $login!";
+            header("Location:index.php");
+        }
+    }
+
+    public static function loadUserById(PDO $conn, int $id) {
+        $query = "SELECT * FROM Users WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([':id' => $id]);
+        if ($result && $stmt->rowCount() == 1) {
+            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $user = new User();
+            $user->id = $userData['id'];
+            $user->username = $userData['username'];
+            $user->email = $userData['email'];
+            $user->hassPasswd = $userData['hash_pass'];
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+
+    public static function loadUserByEmail(PDO $conn, $email) {
+        $query = "SELECT * FROM Users WHERE email = :email";
+        $stmt = $conn->prepare($query);
+        $stmt->execute(['email' => $email]);
+        if ($stmt->rowCount() == 1) {
+            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+            $user = new User();
+            $user->id = $userData['id'];
+            $user->username = $userData['username'];
+            $user->email = $userData['email'];
+            $user->hashPasswd = $userData['hash_pass'];
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+
+    public static function loadAllUsers(PDO $conn) {
+        $query = "SELECT * FROM Users ORDER BY Register_date ASC";
+        $result= $conn->query($query);
+        $users = [];
+        if ($result && $result->rowCount() > 0) {
+            foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $userData) {
+                $user = new User;
+                $user->id = $userData['id'];
+                $user->username = $userData['username'];
+                $user->email = $userData['email'];
+                $user->hashPasswd = $userData['hash_pass'];
+                $users [] = $user;
+            }
+        }
+        return $users;
+    }
 
     public function __construct()
     {
         $this->username = '';
         $this->email = '';
-        $this->hashPass = '';
-    }
-
-    public static function loadAllUsers(PDO $conn)
-
-    {
-        $sql = "SELECT * FROM Users ORDER BY id ASC";
-
-        $result = $conn->query($sql);
-        $users = [];
-
-        if ($result && $result->rowCount() > 0) {
-            foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $userData) {
-                $user = new User();
-
-                $user->id = $userData['id'];
-                $user->username = $userData['username'];
-                $user->email = $userData['email'];
-                $user->hashPass = $userData['hash_pass'];
-
-                $users[] = $user;
-            }
-        }
-
-        return $users;
-    }
-
-    public static function loadUserById(PDO $conn, int $id)
-
-    {
-        $sql = "SELECT * FROM Users WHERE id = :id";
-        $stmt = $conn->prepare($sql);
-        $result = $stmt->execute(['id' => $id]);
-
-        if ($result && $stmt->rowCount() == 1) {
-            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $user = new User();
-
-            $user->id = $userData['id'];
-            $user->username = $userData['username'];
-            $user->email = $userData['email'];
-            $user->hashPass = $userData['hash_pass'];
-
-            return $user;
-        } else {
-            return  null;
-        }
-    }
-
-    public function saveToDB(PDO $conn)
-    {
-        if ($this->id == -1) { //return -1 == $this->>id;
-            //Nowy obiekt robimy insert
-            $sql = "INSERT INTO Users (username, email, hash_pass)
-                VALUES (:username, :email, :hash_pass)";
-
-            $stmt = $conn->prepare($sql);
-
-            $stmt->execute([
-                'username' => $this->username,
-                'email' => $this->email,
-                'hash_pass' => $this->hashPass,
-            ]);
-
-
-            //jesli zapis sie udal
-            $this->id = $conn->lastInsertId();
-
-            return true;
-
-        } else {
-            $sql = "UPDATE Users SET username = :username, email = :email, hash_pass = :hash_pass WHERE id = :id";
-
-            $stmt = $conn->prepare($sql);
-
-            $stmt->execute([
-                ':username' => $this->username,
-                ':email' => $this->email,
-                ':hash_pass' => $this->hashPass,
-                ':id' => $this->id
-            ]);
-        }
-    }
-
-    public function isNew()
-    {
-        return -1 == $this->id;
-    }
-
-
-    public function delete(PDO $conn)
-
-    {
-        if (!$this->isNew()) {
-            $sql = "DELETE FROM Users WHERE id = :id";
-            $stmt = $conn->prepare($sql);
-
-            if ($stmt->execute(['id' => $this->id])) {
-                $this->id = -1;
-
-                return true;
-            }
-        }
-
-        return false;
+        $this->hashPasswd = '';
     }
 
     /**
@@ -131,7 +101,7 @@ class User
     /**
      * @return string
      */
-    public function getUsername(): string
+    public function getUsername()
     {
         return $this->username;
     }
@@ -160,10 +130,75 @@ class User
         $this->email = $email;
     }
 
-    public function setPassword(string $password)
-
+    /**
+     * @param string $hassPasswd
+     */
+    public function setHashPasswd(string $hassPasswd)
     {
-        $this->hashPass = password_hash($password, PASSWORD_BCRYPT);
+        $this->hashPasswd = password_hash($hassPasswd, PASSWORD_BCRYPT);
     }
 
+    public function saveToDB(PDO $conn)
+    {
+        if ($this->id == -1 ) {
+            //Nowy biekt - INSERT
+            $query = "INSERT INTO Users(username, email, hash_pass, Register_date)
+                          VALUES (:username, :email, :hash_pass, NOW())";
+            $stmt = $conn->prepare($query);
+            $stmt->execute([
+                ':username' => $this->username,
+                ':email' => $this->email,
+                ':hash_pass' => $this->hashPasswd,
+            ]);
+            if ($stmt) {
+                $this->id = $conn->lastInsertId();
+            }
+            return true;
+        } else {
+            //Obiekt już istnieje - UPDATE
+            $query = "UPDATE Users SET username = :username, email = :email, hash_pass = :has_pass WHERE id = :id";
+            $stmt = $conn->prepare($query);
+            return $stmt->execute([
+                ':username' => $this->username,
+                ':email' => $this->email,
+                ':hash_pass' => $this->hassPasswd,
+                ':id' => $this->id
+            ]);
+        }
+    }
+
+    public function delete(PDO $conn) {
+        if($this->id != -1) {
+            $query = "DELETE FROM Users WHERE id = :id";
+            $stmt= $conn->prepare($query);
+            if($stmt->execute([':id' => $this->id])) {
+                $this->id = -1;
+                return true;
+            }
+        }
+    }
+
+    public function updateData(PDO $conn, User $newUser) {
+        $query = "UPDATE Users SET username=:username WHERE email=:email";
+        $stmt = $conn->prepare($query);
+        $result = $stmt->execute([
+            ':username' => $newUser->username,
+            ':email' => $newUser->email
+        ]);
+        if ($result) {
+            echo 'Dane zostały zaktualizowane';
+        }
+    }
+
+    public function updatePassword(PDO $conn, User $newUser) {
+        $query = "UPDATE Users SET hash_pass=:password WHERE id=:id";
+        $stmt = $conn->prepare($query);
+        $result =$stmt->execute([
+            ':password' => $newUser->hashPasswd,
+            ':id' => $newUser->id
+        ]);
+        if ($result) {
+            echo 'Hasło zostało zmienione';
+        }
+    }
 }
